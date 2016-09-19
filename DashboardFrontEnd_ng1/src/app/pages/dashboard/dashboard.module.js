17770378325efd42b8e602b313e2dd87d9b99683
Mahmoud Ali El-Zouhery <Mahmoud Ali El-Zouhery>
2016-09-19 11:09:57 -0700

    Fixes

--- DashboardFrontEnd_ng1/src/app/pages/dashboard/dashboard.module.js
+++ DashboardFrontEnd_ng1/src/app/pages/dashboard/dashboard.module.js
@@ -10,7 +10,7 @@
         .controller('DashboardCtrl', ['$scope', '$interval', 'almService', DashboardCtrl]);
 
     function DashboardCtrl($scope, $interval, almService) {
-       /* var stop = $interval(function () {
+        var stop = $interval(function () {
             console.log("Run Database update", new Date());
             almService.updateDatabase().then(function (result) {
                 if (result > 0) {
@@ -18,7 +18,7 @@
                     almService.notify();
                 }
             });
-        }, 120000);*/
+        }, 600000);
         
         almService.subscribe($scope, function () {
             console.log("Load line chart");
