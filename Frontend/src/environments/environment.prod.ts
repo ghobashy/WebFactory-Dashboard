47cb417a0b083c15b623dac8e446c034db141eb5
Mahmoud Ali El-Zouhery <info@elzouhery.com>
2017-07-19 11:52:42 +0200

    Update environment file for production

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  oauth: {
+    production: true,
+    oauth: {
         clientID: '0Pm3E1o3rdd0z267iKVizXanT2WTmPms',
         clientKey: 'gUTWGJAYFigpMeP3Rqh5pPqfiafD2__ABs6gDBxICEDBggsQ9Esa6bC-eD6BYTzO',
         grant_type: 'client_credentials',
@@ -8,5 +8,5 @@ export const environment = {
         tokenUrl: "https://elzouhery.auth0.com/oauth/token",
         issuer: "https://elzouhery.auth0.com/"
     },
-    backendUrl: "http://localhost:3000/"
+    backendUrl: "http://www.elzouhery.net/api/"
 };
